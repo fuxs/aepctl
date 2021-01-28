7a9dbb8abb957dd0a288d58b7237a209b9f58f62
fuxs <michael@bungenstock.de>
2021-01-28 13:30:49 +0100

    Removed the example description

--- cmd/aepctl.go
+++ cmd/aepctl.go
@@ -31,12 +31,9 @@ import (
 
 var (
 	longDesc = util.LongDesc(`
-	A longer description that spans multiple lines and likely contains
-	examples and usage of using your application. For example:
+	The command line tool for AEP
 	
-	Cobra is a CLI library for Go that empowers applications.
-	This application is a tool to generate the needed files
-	to quickly create a Cobra application.`)
+	aepctl is a command line tool for the Adobe Experience Platform implementing a part of the REST API.`)
 )
 
 // NewCommand return an initialized command
@@ -44,7 +41,7 @@ func NewCommand() *cobra.Command {
 	cobra.OnInitialize(initialze)
 	cmd := &cobra.Command{
 		Use:                   "aepctl",
-		Short:                 "A brief description of your application",
+		Short:                 "The command line tool for AEP",
 		Long:                  longDesc,
 		DisableFlagsInUseLine: true,
 	}
