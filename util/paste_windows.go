24edcedecfda200d4591e5fa0b62e5dc2b6788f4
fuxs <michael@bungenstock.de>
2021-02-12 21:12:17 +0100

    Adds missing paste implementation

---
+++ util/paste_windows.go
@@ -0,0 +1,81 @@
+/*
+Package util util consists of general utility functions and structures.
+
+Copyright 2021 Michael Bungenstock
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+*/
+// +build windows
+package util
+
+import (
+	"syscall"
+	"time"
+	"unsafe"
+)
+
+var (
+	user32           = syscall.MustLoadDLL("user32")
+	openClipboard    = user32.MustFindProc("OpenClipboard")
+	closeClipboard   = user32.MustFindProc("CloseClipboard")
+	getClipboardData = user32.MustFindProc("GetClipboardData")
+
+	kernel32     = syscall.NewLazyDLL("kernel32")
+	globalLock   = kernel32.NewProc("GlobalLock")
+	globalUnlock = kernel32.NewProc("GlobalUnlock")
+)
+
+const (
+	cfUnicodetext = 13
+)
+
+func waitOpenClipboard() error {
+	started := time.Now()
+	limit := started.Add(time.Second)
+	var r uintptr
+	var err error
+	for time.Now().Before(limit) {
+		r, _, err = openClipboard.Call(0)
+		if r != 0 {
+			return nil
+		}
+		time.Sleep(time.Millisecond)
+	}
+	return err
+}
+
+func paste() string {
+	err := waitOpenClipboard()
+	if err != nil {
+		return ""
+	}
+	defer closeClipboard.Call()
+
+	h, _, err := getClipboardData.Call(cfUnicodetext)
+	if h == 0 {
+		return ""
+	}
+
+	l, _, err := globalLock.Call(h)
+	if l == 0 {
+		return ""
+	}
+
+	text := syscall.UTF16ToString((*[1 << 20]uint16)(unsafe.Pointer(l))[:])
+
+	r, _, err := globalUnlock.Call(h)
+	if r == 0 {
+		return ""
+	}
+
+	return text
+}
